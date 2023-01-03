84448ad542b75ee4c2e4e719e52930c94e8551dd
Tiago <tiago.rainho@ua.pt>
2023-01-03 21:56:57 +0000

    minor cleanup of map service

--- src/services/map_service.py
+++ src/services/map_service.py
@@ -6,18 +6,11 @@ from sprites.map_sprite import MapSprite
 from services.dog_service import DogService
 from services.sheep_service import SheepService
 from services import service_locator
-from classes.vector import Vector
 from models.map_constraints import MapConstraints
 
 
-
 class MapService:
-    # change to use a map [coordinates, sheep]
     sprites = pygame.sprite.Group
-
-    dog_service: DogService
-    sheep_service: SheepService
-
     map_constraints: MapConstraints
 
     def __init__(self, map_constraints: MapConstraints):
@@ -26,11 +19,10 @@ class MapService:
         self.sprites = pygame.sprite.Group()
         self.sprites.add(MapSprite())
     
-    
     def update(self):
-        self.dog_service: DogService = service_locator.get_service(DogService.__name__)
-        self.sheep_service: SheepService = service_locator.get_service(SheepService.__name__)
+        dog_service: DogService = service_locator.get_service(DogService.__name__)
+        sheep_service: SheepService = service_locator.get_service(SheepService.__name__)
 
         # constraint objects
-        self.map_constraints.update(self.dog_service.dogs)
-        self.map_constraints.update(self.sheep_service.sheeps)
+        self.map_constraints.update(dog_service.dogs)
+        self.map_constraints.update(sheep_service.sheeps)
