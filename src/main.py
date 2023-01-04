78f2a96cae54726a1c930a0be40a130f3ed0da65
Tiago <tiago.rainho@ua.pt>
2023-01-04 16:57:43 +0000

    comment cProfile handler functions

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
+from game.sheep_game import SheepGame
+
 import cProfile
 import atexit
 import pstats
 import io
 
-from game.sheep_game import SheepGame
-
 DEBUG: bool = False
-
 pr: cProfile.Profile
 
 def exit_handler():
+    """
+    Handle exit to log performance stats from the game.
+    """
+    
     if not DEBUG: return
     s = io.StringIO()
     ps = pstats.Stats(pr, stream=s).sort_stats('tottime')
@@ -20,6 +23,7 @@ def exit_handler():
 
 if __name__ == '__main__':
 
+    # start the register of performance metrics
     if DEBUG:
         atexit.register(exit_handler)
         pr = cProfile.Profile()
@@ -28,5 +32,6 @@ if __name__ == '__main__':
     # create and start the simulation
     SheepGame().start()
 
+    # process performance metrics
     if DEBUG:
         pr.disable()
